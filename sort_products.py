f71a6e1659e22f1b07be749d366368ff90c33d28
MoHan <balamurugun530@gmail.com>
2026-01-09 10:08:14 +0530

    Convert JavaScript product sorting to Python

--- sort_products.py
+++ sort_products.py
@@ -1,14 +1,15 @@
-// Array of product objects
-const products = [
-    { name: "Eco-Friendly Bottle", price: 25 },
-    { name: "Bamboo Toothbrush", price: 10 },
-    { name: "Solar Charger", price: 50 },
-    { name: "Organic Tote Bag", price: 15 },
-    { name: "Recycled Notebook", price: 30 }
-];
+# List of product dictionaries
+products = [
+    {"name": "Eco-Friendly Bottle", "price": 25},
+    {"name": "Bamboo Toothbrush", "price": 10},
+    {"name": "Solar Charger", "price": 50},
+    {"name": "Organic Tote Bag", "price": 15},
+    {"name": "Recycled Notebook", "price": 30}
+]
 
-// Sort products by price in ascending order
-products.sort((a, b) => a.price - b.price);
+# Sort using a lambda function as the key
+products.sort(key=lambda x: x['price'])
 
-console.log("Products sorted by price (Low to High):");
-console.log(products);
+print("Products sorted by price (Low to High):")
+for item in products:
+    print(f"{item['name']}: ${item['price']}")
